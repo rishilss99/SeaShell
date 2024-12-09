33df903cda45782c9b4da9dd2073a39d0cae068d
rishilss99 <rishil2008@gmail.com>
2024-12-09 10:28:36 -0800

    Invalid command

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+#include <unordered_set>
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  // Supported commands
+  std::unordered_set<std::string> supported_commands;
+
   // Uncomment this block to pass the first stage
   std::cout << "$ ";
   
   std::string input;
   std::getline(std::cin, input);
+
+  if(supported_commands.find(input) == supported_commands.end())
+  {
+    std::cout << input << ": command not found\n";
+    std::exit(1);
+  }
+
+  return 0;
 }
