27b2f43b80b284b6912543f0f3990574052c1939
rishilss99 <rishil2008@gmail.com>
2024-12-09 10:50:06 -0800

    Exit 0

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_set>
+#include <sstream>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -7,16 +8,19 @@ int main() {
   std::cerr << std::unitbuf;
 
   // Supported commands
-  std::unordered_set<std::string> supported_commands;
+  std::unordered_set<std::string> supported_commands{"exit"};
 
   // Uncomment this block to pass the first stage
   std::string input;
+  std::string command;
 
   while (true)
   {
     std::cout << "$ ";
     std::getline(std::cin, input);
-    if (supported_commands.find(input) == supported_commands.end())
+    std::stringstream ss(input);
+    ss >> command;
+    if (supported_commands.find(command) == supported_commands.end())
     {
       std::cout << input << ": command not found\n";
     }
